cb2a7068c6cc8465e940e80fd9179140ba9736a1
Aakash Rao <mrao.aakash@gmail.com>
2023-07-21 12:38:55 +0530

    added redundancies

--- vis.py
+++ vis.py
@@ -5,17 +5,20 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-fish_name = 'laila'
+fish_name = 'M2'
 
 
 pre = pd.read_csv(f'./data/{fish_name}_pre.csv')
-post = pd.read_csv(f'./data/{fish_name}_post.csv')
+try:
+    post = pd.read_csv(f'./data/{fish_name}_post.csv')
+except:
+    print(f'No post-intervention data for this {fish_name} fish')
 print(pre.head())
 
 # Plotting the distribution of the data
 
 plt.figure(figsize=(5,3), dpi=300)
-plt.title(f'Pre/Post-intervention for fish {fish_name}', fontsize=16, fontweight='bold')
+plt.title(f'Pre-intervention for fish {fish_name}', fontsize=16, fontweight='bold')
 # plt.title('Post-intervention for fish {fish_name}', fontsize=16, fontweight='bold')
 plt.plot(pre['Time.1'], pre['Readings'], '-', color='blue', label='Pre-intervention')
 plt.xticks(np.arange(0,120,10),rotation=90, fontsize=8)
@@ -25,7 +28,20 @@ plt.ylabel('DO Readings (ppm)', fontsize=12, fontweight='bold')
 plt.tight_layout()
 # plt.show()
 plt.savefig(f'./vis/{fish_name}_pre.png')
-
+try:
+    plt.figure(figsize=(5,3), dpi=300)
+    plt.title(f'Post-intervention for fish {fish_name}', fontsize=16, fontweight='bold')
+    # plt.title('Post-intervention for fish {fish_name}', fontsize=16, fontweight='bold')
+    plt.plot(post['Time.1'], post['Readings'], '-', color='red', label='Post-intervention')
+    plt.xticks(np.arange(0,120,10),rotation=90, fontsize=8)
+    plt.ylim(0, 10)
+    plt.xlabel('Time (per minute)', fontsize=12, fontweight='bold')
+    plt.ylabel('DO Readings (ppm)', fontsize=12, fontweight='bold')
+    plt.tight_layout()
+    # plt.show()
+    plt.savefig(f'./vis/{fish_name}_post.png')
+except:
+    print(f'No post-intervention data for this {fish_name} fish')
 
 plt.figure(figsize=(5,3), dpi=300)
 plt.title(f'Pre/Post-intervention for fish {fish_name}', fontsize=16, fontweight='bold')
@@ -35,8 +51,10 @@ plt.xticks(np.arange(0,120,10),rotation=90, fontsize=8)
 plt.ylim(0, 10)
 plt.xlabel('Time (per minute)', fontsize=12, fontweight='bold')
 plt.ylabel('DO Readings (ppm)', fontsize=12, fontweight='bold')
-
-plt.plot(post['Time.1'], post['Readings'], '-', color='red', label='Post-intervention')
+try:
+    plt.plot(post['Time.1'], post['Readings'], '-', color='red', label='Post-intervention')
+except:
+    print(f'No post-intervention data for this {fish_name} fish')
 plt.legend(loc='upper right', fontsize=8)
 
 plt.tight_layout()
